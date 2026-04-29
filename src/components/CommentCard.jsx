9e480a193b01702c2aa510fff230162cd955467d
Arian Aziri <aa32814@seeu.edu.mk>
2026-04-29 12:09:47 +0200

    Added CommentCard

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,27 @@
+function CommentCard({comment})  {
+    let isTopRated = comment.rating >= 4;
+    return <>
+        <div>
+            <p>
+            Id: {comment.id}
+            </p>
+            <p>
+            Name: {comment.name}
+            </p>
+            <p>
+            Email: {comment.email}
+            </p>
+            <p>
+            Body: {comment.body}
+            </p>
+            <p>
+            rating: {comment.rating}
+            {isTopRated && <div> Your comment is top rated</div>}
+            </p>
+            <p>
+            approved: {comment.approved}
+            {comment.approved && <div>Your comment is approved</div>}
+            </p>
+        </div>
+    </>
+}
